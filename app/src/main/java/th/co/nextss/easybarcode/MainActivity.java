4c7efe73fcaf732bfce3bb8ca38bdf41a8672ae9
NextDevNoteBook <panuwat.a@nextss.co.th>
2017-12-18 11:56:26 +0700

    Design UI main success.

--- app/src/main/java/th/co/nextss/easybarcode/MainActivity.java
+++ app/src/main/java/th/co/nextss/easybarcode/MainActivity.java
@@ -3,11 +3,22 @@ package th.co.nextss.easybarcode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import th.co.nextss.easybarcode.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+
+        }
+    }   // Main Method
+
+}   // Main Class
---
+++ app/src/main/java/th/co/nextss/easybarcode/fragment/MainFragment.java
@@ -0,0 +1,27 @@
+package th.co.nextss.easybarcode.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import th.co.nextss.easybarcode.R;
+
+/**
+ * Created by nextdev on 18-Dec-17.
+ */
+
+public class MainFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return view;
+    }
+}   // Main Class
